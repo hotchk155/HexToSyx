4f180abbe91fa6de9c7983ce9b43f17d1bdf8517
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2016-05-06 15:11:39 +0100

    stuff

--- HexToSyx/HexToSyx.cpp
+++ HexToSyx/HexToSyx.cpp
@@ -241,7 +241,7 @@ bool process_file(FILE *infile, FILE *outfile)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
+	if (argc != 3) {
 		printf("Use: hex2syx <input.hex> <output.syx>\n");
 		exit(1);
 	}
